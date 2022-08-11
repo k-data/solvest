b4dc00787aa18af02f6b22d4164fe973cd4862bf
k-data <83756659+k-data@users.noreply.github.com>
2022-08-11 19:13:27 +0900

    test5

--- streamlit/python/main_streamlit.py
+++ streamlit/python/main_streamlit.py
@@ -217,7 +217,7 @@ elif choice == "ログイン":
 								df1_sum = df1_sum.loc[options]
 								
 								value = '正味重量_明細'
-								st.write(df1_sum.iloc[:,:2])
+								st.write(df1_sum.loc[:,['正味重量_明細','金額']])
 								st.bar_chart(df1_sum.iloc[:,0])
 								fig = px.pie(df1_sum, values=value, names='商品')
 								fig.update_layout(margin=dict(t=20, b=20, l=20, r=20))
